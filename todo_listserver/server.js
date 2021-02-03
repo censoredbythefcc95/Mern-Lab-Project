7af3f9149b7fcaad556f6e216301bbcf17e92ebb
Steven Young <stevenyoung@stevens-mbp.myfiosgateway.com>
2021-02-03 12:03:36 -0500

    test deploy

--- todo_listserver/server.js
+++ todo_listserver/server.js
@@ -1,1 +1,12 @@
 const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Express server listening on port ${PORT}`);
+});
+
+app.get('/', (req, res)=>{
+    res.send('here is your information');
+  });
